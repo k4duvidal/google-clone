9869256e7440cbc76b7eb04103a15a9a6b3e050c
k4duvidal <k4duvidal@gmail.com>
2021-09-27 23:44:07 -0300

    add function pesquisar wiki, add keydown ao input

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
+document.getElementById('entrada').addEventListener(
+    "keydown", function(evento){
+        if(evento.keyCode == 13){
+            pesquisaGoogle()
+        }
+    }
+)
+
 
 function pesquisaGoogle(){
     url = "https://www.google.com/search?q="
     valordigitado = document.getElementById('entrada').value
     pesquisa = url + valordigitado;
     window.open(pesquisa, "_self")
+}
+
+function pesquisaWiki(){
+    url = "https://pt.wikipedia.org/wiki/"
+    valordigitado = document.getElementById('entrada').value
+    pesquisa = url + valordigitado;
+    window.open(pesquisa, "_self")
+}
+
+function fazerLogin(){
+    window.open("https://accounts.google.com/ServiceLogin?hl=pt-BR&passive=true&continue=https://www.google.com/%3Fgws_rd%3Dssl%26pccc%3D1&ec=GAZAmgQ", "_self")
+
 }
