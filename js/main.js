2dd41904269ced26979e8497ae84c9bf8446c87e
k4duvidal <k4duvidal@gmail.com>
2021-09-29 19:29:02 -0300

    add function limpar input

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ document.getElementById('entrada').addEventListener(
     }
 )
 
-
 function pesquisaGoogle(){
     url = "https://www.google.com/search?q="
     valordigitado = document.getElementById('entrada').value
@@ -24,4 +23,9 @@ function pesquisaWiki(){
 function fazerLogin(){
     window.open("https://accounts.google.com/ServiceLogin?hl=pt-BR&passive=true&continue=https://www.google.com/%3Fgws_rd%3Dssl%26pccc%3D1&ec=GAZAmgQ", "_self")
 
+}
+
+
+function botaolimpar(){
+    document.getElementById('entrada').value = ""
 }
